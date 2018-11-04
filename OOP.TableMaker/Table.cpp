bd5f52ac537c149299078a79fe34a70e38826299
Kirill  Makharadze <kirill.madze@gmail.com>
2018-11-04 19:58:39 +0300

    Initial Commit

---
+++ OOP.TableMaker/Table.cpp
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+#include "Table.h"
+
+std::string hello = "hello!";
+
+Table::Table()
+{
+}
+
+
+Table::~Table()
+{
+}
+
+std::string Table::get_hello()
+{
+	return hello;
+}
---
+++ OOP.TableMaker/Table.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <string>
+
+class Table
+{
+public:
+	Table();
+	~Table();
+	std::string get_hello();
+};
+
