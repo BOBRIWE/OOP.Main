68b5f593d665c900615972afc5a73c7eb173b2f5
Kirill  Makharadze <kirill.madze@gmail.com>
2018-11-11 17:39:54 +0300

    12.3 Done

--- OOP.12.2/Rect.h
+++ OOP.12.2/Rect.h
@@ -9,16 +9,7 @@ public:
 	{
 		set_height(height);
 		set_width(width);
-		_pt[0] = base_point;
-
-		_pt[1].x = base_point.x + width;
-		_pt[1].y = base_point.y;
-
-		_pt[2].x = base_point.x + width;;
-		_pt[2].y = base_point.y + height;
-
-		_pt[3].x = base_point.x;
-		_pt[3].y = base_point.y + height;
+		reset_base_point(base_point);
 	}
 
 	void draw()
@@ -31,29 +22,38 @@ public:
 		Polygon(hdc, _pt, 4);
 		DeleteObject(pen);
 	}
-};
 
-//
-//HPEN hPolygonPen;
-//COLORREF qPolygonColor;
-//qPolygonColor = RGB(255, 0, 0);
-//hPolygonPen = CreatePen(PS_SOLID, 7, qPolygonColor);
-//
-//POINT Pt[4];
-//Pt[0].x = 10;
-//Pt[0].y = 20;
-//
-//Pt[1].x = 100;
-//Pt[1].y = 0;
-//
-//Pt[2].x = 100;
-//Pt[2].y = 100;
-//
-//Pt[3].x = 0;
-//Pt[3].y = 100;
-//
-//
-//Polygon(hdc, Pt, 4);
-//
-//SelectObject(hdc, hPenOld);
-//DeleteObject(hPolygonPen);
+	void reset_width(int w)
+	{
+		width = w;
+		reset_base_point(base_point);
+	}
+
+	void reset_height(int h)
+	{
+		height = h;
+		reset_base_point(base_point);
+	}
+
+	void reset_color(COLORREF c)
+	{
+		color = c;
+		pen = CreatePen(PS_SOLID, 7, color);
+		brush = CreateSolidBrush(color);
+	}
+
+	void reset_base_point(POINT pt)
+	{
+		base_point = pt;
+		_pt[0] = base_point;
+
+		_pt[1].x = base_point.x + width;
+		_pt[1].y = base_point.y;
+
+		_pt[2].x = base_point.x + width;;
+		_pt[2].y = base_point.y + height;
+
+		_pt[3].x = base_point.x;
+		_pt[3].y = base_point.y + height;
+	}
+};
